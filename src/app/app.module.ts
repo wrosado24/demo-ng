af9e2c40a4635f4e0c50c80aca1b5738bc163cc8
Walter Rosado <wrosado.ti@sacooliveros.edu.pe>
2021-08-03 11:26:23 -0500

    add primeng and primeicons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccountListComponent } from './account/account-list/account-list.component';
-import { AccountDeleteComponent } from './account/account-delete/account-delete.component';
-import { AccountCreateComponent } from './account/account-create/account-create.component';
-import { AccountUpdateComponent } from './account/account-update/account-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AccountListComponent,
-    AccountDeleteComponent,
-    AccountCreateComponent,
-    AccountUpdateComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
