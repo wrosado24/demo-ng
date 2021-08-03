7a1ebfd72eb168a50c2098a30b2c321a9b787275
Walter Rosado <wrosado.ti@sacooliveros.edu.pe>
2021-08-03 01:18:04 -0500

    four crud components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountListComponent } from './account/account-list/account-list.component';
+import { AccountDeleteComponent } from './account/account-delete/account-delete.component';
+import { AccountCreateComponent } from './account/account-create/account-create.component';
+import { AccountUpdateComponent } from './account/account-update/account-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountListComponent,
+    AccountDeleteComponent,
+    AccountCreateComponent,
+    AccountUpdateComponent
   ],
   imports: [
     BrowserModule,
